ef8f84e9e1d3791a3c6790478b8bb600bef4a12e
kvkcon <2300271042@email.szu.edu.cn>
2025-04-07 17:03:32 +0800

    fix: rm build dir

--- x1_urdf/build/x1_urdf/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lx/projects/x1_urdf
-# Build directory: /home/lx/projects/x1_urdf/build/x1_urdf
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
